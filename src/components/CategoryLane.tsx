2d9d23aff2f953de112fd0e65b87945461bcc9bb
Adrian Hegedus <adrian.hegedus@visma.com>
2023-09-27 23:00:55 +0200

    Init

--- src/components/CategoryLane.tsx
+++ src/components/CategoryLane.tsx
@@ -7,21 +7,18 @@ export const CategoryLane = ({ category, tasks, actions }: CategoryLaneProps) =>
 	const bgClass = `bg-${category.color}-500`
 	const textClass = `text-${category.color}-800`
 	return (
-		<div className={`p-5 ${bgClass} bg-opacity-5 rounded-xl flex grow basis-0 flex-col gap-5`}>
-			<div className={`flex justify-between font-title items-center`}>
-				<span className={`font-medium text-neutral-800 text-xl`}>{category.name}</span>
-				<span className={`p-2 ${bgClass} ${textClass} bg-opacity-5 rounded-md text-xs font-medium`}>{tasks.length}</span>
+		<div className={`${bgClass} rounded-xl flex grow basis-0 flex-col gap-5`}>
+			<div>
+				<span>{category.name}</span>
+				<span>{tasks.length}</span>
 			</div>
 
-			<button
-				className={`p-2 flex grow ${bgClass} ${textClass} font-title bg-opacity-5 hover:bg-opacity-10 rounded-md text-xs font-medium justify-center items-center gap-1`}
-				onClick={() => actions.add(category.id)}
-			>
+			<button onClick={() => actions.add(category.id)}>
 				<BiPlus />
 				New
 			</button>
 
-			<div className={"flex flex-col gap-5"}>
+			<div>
 				{tasks.sort(sortTasksByStatusAndDeadline)
 				      .map(task => {
 						  return <Task task={task} actions={actions} key={task.id} />
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -4,21 +4,21 @@ import { TaskProps } from "~/data/types";
 
 export const Task = ({ task, actions }: TaskProps) => {
 	return (
-		<div className={`bg-white p-5 rounded-md font-body flex flex-col gap-1 shadow-md`}>
-			<div className={"font-medium flex items-center gap-2"}>
-				<div className={"cursor-pointer"} onClick={() => actions.toggle(task)}>
+		<div>
+			<div>
+				<div onClick={() => actions.toggle(task)}>
 					{task.done ? <BiSolidCheckCircle /> : <BiCircle />}
 				</div>
-				<div className={"flex justify-between w-full"}>
-					<span className={"font-medium " + (task.done ? "line-through" : "")}>{task.name}</span>
-					<div className={"flex gap-1 items-center"}>
-						<button className={"opacity-50 hover:opacity-100"} onClick={() => actions.edit(task)}><BiPencil/></button>
-						<button className={"opacity-50 hover:opacity-100"} onClick={() => actions.delete(task)}><BiX/></button>
+				<div>
+					<span>{task.name}</span>
+					<div>
+						<button onClick={() => actions.edit(task)}><BiPencil/></button>
+						<button onClick={() => actions.delete(task)}><BiX/></button>
 					</div>
 				</div>
 			</div>
 			{task.deadline &&
-				<div className={"flex items-center gap-1 ml-6 text-xs opacity-75"}>
+				<div>
 					<BiTimeFive />
 					<div>{new Date(task.deadline).toLocaleDateString()}</div>
 				</div>
